0985a496359cc994f23dd7c6230159c3bfbf959a
성완 <cjddktkdwk@naver.com>
2024-12-26 05:54:42 +0900

    Feat : menubar
    - 메뉴바 모바일 화면 레이아웃 안에서 움직이게 변경
    - 메뉴바 아이콘 등 css 추가/변경

--- front/src/pages/MainPage.js
+++ front/src/pages/MainPage.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import MainImage from "../image/동백꽃바라.png";
 import { useNavigate } from "react-router-dom";
+import {
+  IoSettingsOutline,
+  IoMailOutline,
+  IoChatbubblesOutline,
+} from "react-icons/io5";
+import { AiOutlineAlert } from "react-icons/ai";
+import PocketIcon from "../image/pocketIcon.svg";
 
 const MainPage = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -9,7 +16,7 @@ const MainPage = () => {
   const toggleMenu = () => setMenuOpen((prev) => !prev);
 
   return (
-    <div className="relative flex flex-col items-center justify-center w-full max-w-[375px] min-h-screen bg-[#0d1a26] text-white">
+    <div className="relative flex flex-col items-center justify-center w-full max-w-[375px] min-h-screen bg-[#0d1a26] text-white overflow-hidden">
       {/* 오버레이 */}
       {menuOpen && (
         <div
@@ -28,17 +35,37 @@ const MainPage = () => {
 
       {/* 메뉴 바 */}
       <div
-        className={`fixed top-0 right-0 h-full bg-[#333] text-white shadow-lg transition-transform duration-300 ease-in-out z-20 ${
+        className={`text-lg absolute top-0 left-28 h-full bg-[#333] text-white shadow-lg transition-transform duration-300 ease-in-out z-20 ${
           menuOpen ? "translate-x-0" : "translate-x-full"
         }`}
-        style={{ width: "175px" }}
+        style={{ width: "275px" }}
       >
-        <ul className="p-4 space-y-4">
-          <li className="hover:underline cursor-pointer">메뉴1</li>
-          <li className="hover:underline cursor-pointer">메뉴2</li>
-          <li className="hover:underline cursor-pointer">메뉴3</li>
-          <li className="hover:underline cursor-pointer">메뉴4</li>
-          <li className="hover:underline cursor-pointer">메뉴5</li>
+        <ul className="py-3 px-6 space-y-4">
+          <button className="flex hover:underline items-center gap-2">
+            <IoSettingsOutline />
+            <span className="mb-1">계정 설정</span>
+          </button>
+          <button className="flex hover:underline items-center gap-2">
+            <img
+              src={PocketIcon}
+              alt="pocketIcon"
+              width="18"
+              className="mb-1"
+            ></img>
+            <span className="mb-1">내 복주머니 보러가기</span>
+          </button>
+          <button className="flex hover:underline items-center gap-2">
+            <IoMailOutline />
+            <span className="mb-1">내가 보낸 메시지</span>
+          </button>
+          <button className="flex hover:underline items-center gap-2">
+            <IoChatbubblesOutline />
+            <span className="mb-1">피드백하기</span>
+          </button>
+          <button className="flex hover:underline items-center gap-2">
+            <AiOutlineAlert className="mb-1" />
+            <span className="mb-1">신고하기</span>
+          </button>
         </ul>
         <footer className="border-t border-gray-600 p-4 text-center text-sm">
           Lucky Bocky!
