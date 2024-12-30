e2961148f8efe9dab0c925837bdaa54a353af25e
Jo-dv <whgjsgh11@gmail.com>
2024-12-31 00:22:34 +0900

    Revert "Merge branch 'back' into front"
    This reverts commit f0a7689e8cf585deb592576accca319781fb24e2, reversing
    changes made to 688108eb08e0740c9f024664329c7deb8da3d40c.

--- back/luckybocky/src/main/java/com/project/luckybocky/common/WebMvcConfig.java
+++ back/luckybocky/src/main/java/com/project/luckybocky/common/WebMvcConfig.java
@@ -1,6 +1,5 @@
 package com.project.luckybocky.common;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,9 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-	@Value("${front_uri}")
-	private String frontUri;
-
 //	@Override
 //	public void addInterceptors(InterceptorRegistry registry) {
 //		registry.addInterceptor(sessionInterceptor())
@@ -21,7 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins(frontUri)
+		registry.addMapping("/**").allowedOrigins("http://localhost:3000/")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
 			.allowedHeaders("*")
 			.allowCredentials(true).maxAge(3000);
