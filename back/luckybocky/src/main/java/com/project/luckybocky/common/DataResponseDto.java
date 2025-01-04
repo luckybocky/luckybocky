e4b4ffe31329fe6fddda6d5b320ba71f6ef8bc64
changhi97 <77597957+changhi97@users.noreply.github.com>
2025-01-05 00:19:15 +0900

    Feat : ResponseDto, DataResponseDto 추가

--- back/luckybocky/src/main/java/com/project/luckybocky/common/DataResponseDto.java
+++ back/luckybocky/src/main/java/com/project/luckybocky/common/DataResponseDto.java
@@ -1,14 +1,10 @@
 package com.project.luckybocky.common;
 
-
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
-@Builder
 @Getter
 @AllArgsConstructor
-public class MessageDto {
-    private String status;
-    private String message;
+public class DataResponseDto<T> extends ResponseDto {
+    private T data;
 }
--- back/luckybocky/src/main/java/com/project/luckybocky/common/ResponseDto.java
+++ back/luckybocky/src/main/java/com/project/luckybocky/common/ResponseDto.java
@@ -1,11 +1,14 @@
 package com.project.luckybocky.common;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Builder
 @Getter
 @AllArgsConstructor
-public class ErrorResponse {
-    private int status;
+@NoArgsConstructor
+public class ResponseDto {
     private String message;
 }
