c570b24ad8dffe52f3584a5ab64f0883dc41d44a
Jo-dv <whgjsgh11@gmail.com>
2025-01-13 01:13:30 +0900

    Fix: modified_at 컬럼 변경되지 않는 문제 수정

--- back/luckybocky/src/main/java/com/project/luckybocky/common/BaseEntity.java
+++ back/luckybocky/src/main/java/com/project/luckybocky/common/BaseEntity.java
@@ -20,7 +20,7 @@ public class BaseEntity {
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Column(updatable = false)
+    @Column()
     private LocalDateTime modifiedAt;
 
     @ColumnDefault("false")
