f72869335b33a734db5d61bbfaa8098aebe7250d
성완 <cjddktkdwk@naver.com>
2025-01-15 01:10:29 +0900

    Refactor : 주석 제거

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,10 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
-// import bgImage from "./image/background.png";
 
 const App = () => {
   return (
-    <div
-      className="flex flex-col items-center justify-center bg-[#ede0d4] text-white min-h-screen text-base"
-      // style={{
-      //   backgroundImage: `url(${bgImage})`,
-      //   backgroundSize: "375px 500px", // 배경 이미지를 100x100px로 설정
-      //   backgroundRepeat: "repeat", // 배경 이미지 반복
-      //   backgroundPosition: "center", // 배경 이미지 중앙 정렬
-      //   minHeight: "100vh",
-      // }}
-    >
+    <div className="flex flex-col items-center justify-center bg-[#ede0d4] text-white min-h-screen text-base">
       {/* 라우터 적용 */}
       <RouterProvider router={router} />
     </div>
