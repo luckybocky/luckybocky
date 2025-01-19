191cc58a5d311f5fd76d61202edbb642da9a3be6
jaewonwi <jww5555@naver.com>
2025-01-20 02:12:06 +0900

    Design: Change design account setting page

--- front/src/pages/AccountPage.js
+++ front/src/pages/AccountPage.js
@@ -95,10 +95,10 @@ const AccountPage = () => {
   }
 
   return (
-    <div className="relative flex flex-col w-full max-w-[600px] bg-[#333] p-2">
+    <div className="relative flex flex-col w-full max-w-[600px] bg-[#333] p-6">
       {/* 뒤로 가기 버튼 */}
       <button
-        className="absolute top-4 right-4 text-3xl"
+        className="absolute top-8 right-8 text-3xl"
         onClick={() => navigate(-1)}
       >
         <Suspense>
@@ -108,14 +108,14 @@ const AccountPage = () => {
 
       <h1 className="text-4xl mb-8 mt-5">계정 설정</h1>
       <h1 className="text-2xl">
-        <span className="text-[pink]">{user.userNickname}</span> 님
+        <span className="text-[pink]">{user.userNickname}</span> 님, 새해 복 많이 받으세요 🙂
       </h1>
 
       {/* 구분선 추가 */}
       <hr className="border-t-2 border-gray-600 mt-3 mb-10" />
 
       {/* 닉네임 변경 */}
-      <label className="mb-1">닉네임</label>
+      <label className="text-xl mb-2">닉네임 변경</label>
       <div className="flex items-center">
         <input
           type="text"
@@ -138,7 +138,7 @@ const AccountPage = () => {
             nickname?.length >= 2 && nickname?.length <= 6
               ? "bg-blue-500 hover:bg-blue-600"
               : "bg-gray-400 cursor-not-allowed"
-          } text-white py-2 rounded-lg w-[50px]`}
+          } text-white py-2 rounded-lg w-[100px] p-2`}
           disabled={nickname?.length < 2 || nickname?.length > 6}
         >
           {changeMode ? "저장" : "변경"}
@@ -152,20 +152,23 @@ const AccountPage = () => {
         )}
       </div>
 
-      <hr className="border-t-2 border-gray-600 mt-16 mb-10" />
+      {/* 구분선 추가 */}
+      <hr className="border-t-2 border-gray-600 mt-10 mb-10" />
 
       {/* 알림 설정 여부 */}
-      <div className="flex mb-8">
-        <label className="mr-4">알림 설정 여부</label>
-        <div className="absolute flex items-center right-10">
-          <input
-            type="checkbox"
-            checked={user.alarmStatus}
-            onChange={saveAlarmStatus}
-            className="h-5 w-5"
-          />
-          <span className="ml-2 w-[50px]">
-            {user.alarmStatus ? "허용" : "거절"}
+      <div className="flex mb-6">
+        <label className="w-full md:w-8/12 mr-4">알림 설정 여부</label>
+        <div className="flex justify-between items-center w-full md:w-4/12">
+          <div className="relative w-11 h-5">
+            <input 
+              type="checkbox" 
+              checked={user.alarmStatus}
+              onChange={saveAlarmStatus}
+              className="peer appearance-none w-11 h-5 bg-slate-100 rounded-full checked:bg-blue-500 cursor-pointer transition-colors duration-300" />
+            <label className="absolute top-0 left-0 w-5 h-5 bg-white rounded-full border border-slate-300 shadow-sm transition-transform duration-300 peer-checked:translate-x-6 peer-checked:border-blue-600 cursor-pointer"></label>
+          </div>
+          <span className="ml-2 w-[100px]">
+            {user.alarmStatus ? "알림 허용" : "알림 거절"}
           </span>
         </div>
       </div>
@@ -179,21 +182,23 @@ const AccountPage = () => {
 
       {/* 메시지 공개 여부 */}
       <div className="flex">
-        <label className="mr-4">메시지 공개 여부</label>
-        <div className="absolute flex items-center right-10">
-          <input
-            type="checkbox"
-            checked={user.fortuneVisibility}
-            onChange={saveFortuneVisibility}
-            className="h-5 w-5"
-          />
-          <span className="ml-2 w-[50px]">
-            {user.fortuneVisibility ? "공개" : "비밀"}
+        <label className="w-full md:w-8/12 mr-4">메시지 공개 여부</label>
+        <div className="flex justify-between items-center w-full md:w-4/12">
+          <div className="relative w-11 h-5">
+            <input 
+              type="checkbox" 
+              checked={user.fortuneVisibility}
+              onChange={saveFortuneVisibility}
+              className="peer appearance-none w-11 h-5 bg-slate-100 rounded-full checked:bg-blue-500 cursor-pointer transition-colors duration-300" />
+            <label className="absolute top-0 left-0 w-5 h-5 bg-white rounded-full border border-slate-300 shadow-sm transition-transform duration-300 peer-checked:translate-x-6 peer-checked:border-blue-600 cursor-pointer"></label>
+          </div>
+          <span className="ml-2 w-[100px]">
+            {user.fortuneVisibility ? "전체 공개" : "나만 보기"}
           </span>
         </div>
       </div>
 
-      <hr className="border-t-2 border-gray-600 my-16" />
+      <hr className="border-t-2 border-gray-600 my-10" />
 
       <div>
         <button onClick={logoutButton}>로그아웃</button>
