3a2884603ae425109b5a1d5bd4b4e0be89ebfa93
seongwan lee <cjddktkdwk@naver.com>
2025-01-28 11:57:33 +0900

    Fix : myArticle
    - 공유된 글로 저장된 글은 내가 쓴 글 목록에 안 뜨게 변경

--- back/luckybocky/src/main/java/com/project/luckybocky/article/service/MyArticleServiceImpl.java
+++ back/luckybocky/src/main/java/com/project/luckybocky/article/service/MyArticleServiceImpl.java
@@ -1,43 +1,47 @@
 package com.project.luckybocky.article.service;
 
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.project.luckybocky.article.dto.MyArticleDto;
 import com.project.luckybocky.article.dto.MyArticlesDto;
 import com.project.luckybocky.article.entity.Article;
 import com.project.luckybocky.article.repository.MyArticleRepository;
 import com.project.luckybocky.user.exception.UserNotFoundException;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @Transactional
 @Slf4j
 @RequiredArgsConstructor
 public class MyArticleServiceImpl implements MyArticleService {
-    private final MyArticleRepository myArticleRepository;
+	private final MyArticleRepository myArticleRepository;
 
-    @Override
-    public MyArticlesDto findMyArticles(String userKey) {
+	@Override
+	public MyArticlesDto findMyArticles(String userKey) {
 
-        if(userKey == null){
-            throw new UserNotFoundException();
-        }
+		if (userKey == null) {
+			throw new UserNotFoundException();
+		}
 
-        List<Article> articleByUserKey = myArticleRepository.findArticleByUserKey(userKey);
-        List<MyArticleDto> myArticles = new ArrayList<>();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		List<Article> articleByUserKey = myArticleRepository.findArticleByUserKey(userKey);
+		List<MyArticleDto> myArticles = new ArrayList<>();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-        for (Article article : articleByUserKey) {
-            myArticles.add(
-                article.getMyArticleDto()
-            );
-        }
+		for (Article article : articleByUserKey) {
+			if (article.getShareArticle() == null) {
+				myArticles.add(
+					article.getMyArticleDto()
+				);
+			}
+		}
 
-        return new MyArticlesDto(myArticles);
-    }
+		return new MyArticlesDto(myArticles);
+	}
 }
