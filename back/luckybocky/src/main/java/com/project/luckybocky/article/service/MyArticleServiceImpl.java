27ca07abb10c4299c89682cb8f6231c0aa01826d
changhi97 <77597957+changhi97@users.noreply.github.com>
2025-02-18 18:03:32 +0900

    Feat : 리팩토링

--- back/luckybocky/src/main/java/com/project/luckybocky/article/service/MyArticleServiceImpl.java
+++ back/luckybocky/src/main/java/com/project/luckybocky/article/service/MyArticleServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.project.luckybocky.article.dto.MyArticleDto;
 import com.project.luckybocky.article.dto.MyArticlesDto;
 import com.project.luckybocky.article.entity.Article;
-import com.project.luckybocky.article.repository.MyArticleRepository;
+import com.project.luckybocky.article.repository.ArticleRepository;
 import com.project.luckybocky.user.exception.UserNotFoundException;
 
 import lombok.RequiredArgsConstructor;
@@ -21,7 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 public class MyArticleServiceImpl implements MyArticleService {
-	private final MyArticleRepository myArticleRepository;
+	private final ArticleRepository articleRepository;
 
 	@Override
 	public MyArticlesDto findMyArticles(String userKey) {
@@ -30,7 +30,7 @@ public class MyArticleServiceImpl implements MyArticleService {
 			throw new UserNotFoundException();
 		}
 
-		List<Article> articleByUserKey = myArticleRepository.findArticleByUserKey(userKey);
+		List<Article> articleByUserKey = articleRepository.findByUserUserKey(userKey);
 		List<MyArticleDto> myArticles = new ArrayList<>();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
--- back/luckybocky/src/main/java/com/project/luckybocky/sharearticle/sevice/ShareArticleServiceImpl.java
+++ back/luckybocky/src/main/java/com/project/luckybocky/sharearticle/sevice/ShareArticleServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.project.luckybocky.article.entity.Article;
 import com.project.luckybocky.article.repository.ArticleRepository;
-import com.project.luckybocky.article.repository.MyArticleRepository;
 import com.project.luckybocky.fortune.entity.Fortune;
 import com.project.luckybocky.fortune.exception.FortuneNotFoundException;
 import com.project.luckybocky.fortune.repository.FortuneRepository;
@@ -25,7 +24,7 @@ import com.project.luckybocky.sharearticle.exception.ShareArticleException;
 import com.project.luckybocky.sharearticle.repository.ShareArticleRepository;
 import com.project.luckybocky.user.entity.User;
 import com.project.luckybocky.user.exception.UserNotFoundException;
-import com.project.luckybocky.user.repository.UserSettingRepository;
+import com.project.luckybocky.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -35,25 +34,19 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class ShareArticleServiceImpl implements ShareArticleService {
-	private final UserSettingRepository userSettingRepository;
+	private final UserRepository userRepository;
 	private final FortuneRepository fortuneRepository;
 	private final ShareArticleRepository shareArticleRepository;
-	private final MyArticleRepository myArticleRepository;
 	private final ArticleRepository articleRepository;
 	private final PocketRepository pocketRepository;
 
 	@Override
 	public ShareArticleDto createShareArticle(String userKey, WriteShareArticleDto writeShareArticleDto) {
-		Optional<User> userOptional = userSettingRepository.findByKey(userKey);
-
-		if (userOptional.isEmpty()) {
-			throw new UserNotFoundException();
-		}
+		User user = userRepository.findByUserKey(userKey)
+			.orElseThrow(() -> new UserNotFoundException());
 
 		Fortune fortune = fortuneRepository.findByFortuneSeq(writeShareArticleDto.getFortuneSeq())
 			.orElseThrow(() -> new FortuneNotFoundException());
-
-		User user = userOptional.get();
 
 		UUID uuid = UUID.randomUUID();
 		String address = uuid.toString();
@@ -77,19 +70,12 @@ public class ShareArticleServiceImpl implements ShareArticleService {
 
 	@Override
 	public ShareArticleDto enterShareArticle(String userKey, String shareArticleAddress) {
-		Optional<User> userOptional = userSettingRepository.findByKey(userKey);
+		User user = userRepository.findByUserKey(userKey)
+			.orElseThrow(() -> new UserNotFoundException());
 
-		if (userOptional.isEmpty()) {
-			throw new UserNotFoundException();
-		}
-
-		User user = userOptional.get();
-
-		Optional<ShareArticle> shareArticleOptional = shareArticleRepository.findByAddress(shareArticleAddress);
-		if (shareArticleOptional.isEmpty())
-			throw new ShareArticleException();
-
-		ShareArticle shareArticle = shareArticleOptional.get();
+		ShareArticle shareArticle =
+			shareArticleRepository.findByShareArticleAddress(shareArticleAddress)
+				.orElseThrow(() -> new ShareArticleException());
 
 		if (isMyShareArticle(userKey, shareArticle) || isExistsShareArticle(user, shareArticle)) {
 			log.info("이미 저장되거나 본인의 공유게시글입니다.");
@@ -126,11 +112,10 @@ public class ShareArticleServiceImpl implements ShareArticleService {
 
 	@Override
 	public ShareArticleDto findShareArticle(String shareArticleAddress) {
-		Optional<ShareArticle> shareArticleOptional = shareArticleRepository.findByAddress(shareArticleAddress);
-		if (shareArticleOptional.isEmpty())
-			throw new ShareArticleException();
+		ShareArticle shareArticle = shareArticleRepository.findByShareArticleAddress(shareArticleAddress)
+			.orElseThrow(() -> new ShareArticleException());
 
-		return shareArticleOptional.get().toShareArticleDto();
+		return shareArticle.toShareArticleDto();
 	}
 
 	@Override
@@ -142,12 +127,10 @@ public class ShareArticleServiceImpl implements ShareArticleService {
 	public boolean isExistsShareArticle(User user, ShareArticle shareArticle) {
 		int shareArticleSeq = shareArticle.getShareArticleSeq();
 
-		Optional<Pocket> pocket = pocketRepository.findPocketByUser(user);
+		Pocket pocket = pocketRepository.findPocketByUser(user)
+			.orElseThrow(() -> new PocketNotFoundException());
 
-		if (pocket.isEmpty())
-			throw new PocketNotFoundException();
-
-		List<Article> articles = pocket.get().getArticles();
+		List<Article> articles = pocket.getArticles();
 
 		for (Article article : articles) {
 			ShareArticle findShareArticle = article.getShareArticle();
@@ -165,13 +148,8 @@ public class ShareArticleServiceImpl implements ShareArticleService {
 	public List<ShareArticleDto> getMyShareArticle(String userKey) {
 		List<ShareArticleDto> result = new ArrayList<>();
 
-		Optional<User> userOptional = userSettingRepository.findByKey(userKey);
-
-		if (userOptional.isEmpty()) {
-			throw new UserNotFoundException();
-		}
-
-		User user = userOptional.get();
+		User user = userRepository.findByUserKey(userKey)
+			.orElseThrow(() -> new UserNotFoundException());
 
 		List<ShareArticle> shareArticles = user.getShareArticles();
 
